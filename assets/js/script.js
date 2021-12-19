6dbb9806a1ddd4cd418658b40f5ce6ec351275d8
mmcelwee8002 <mmcelwee8002@gmail.com>
2021-12-19 15:45:21 -0600

    question area complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,22 @@ let time = startingMinutes * 60;
 const timerEl = document.getElementById('timer');
 
 setInterval(updateCowndown, 1000);
+
 function updateCowndown() {
 
     const minutes = Math.floor(time / 60)
 
     var seconds = time % 60
-
+    // this if statement stops the clock at zero 0
+    if (time <= -1) { //added this, checks if time <= -1 and updates variables
+        time = 0;
+        minutes = 0;
+        seconds = 0;
+    }
     seconds = seconds < 10 ? '0' + seconds : seconds;
-
-  timerEl.innerHTML = `${minutes}:${seconds}`;
-  
-
+    timerEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
 }
+
+// Create a new task
+formEl.addEventListener("submit", taskFormHandler);
