489eb1d3585f0984cdcaa918950f71caec9b6d1e
mmcelwee8002 <mmcelwee8002@gmail.com>
2021-12-22 15:41:46 -0600

    questions in javascript set

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,16 +23,16 @@ function updateCowndown() {
     time--;
 }
 
-//question #1
+// questions Objects
 var questions= 
 [
 {
-question:  "what is...?",
-answer: "the answer is...",
-button1: "the answer is...",
-button2: "Hi Jordan",
-button3: "button 3",
-button4: "button 4",
+question:  "What is the name of Minnesota's Football team?",
+answer: "Vikings",
+button1: "Twins",
+button2: "Wild",
+button3: "Vikings",
+button4: "Timberwolves",
 },
 {
   question:  "what is...?",
@@ -44,73 +44,46 @@ button4: "button 4",
   }
 ]
 
-document.getElementById("questions").innerHTML =
-questions[0].answer;
-
-document.getElementById("questions").innerHTML = 
-questions[0].button1;
-
-// questions[0].question1
-
-console.log(Object.keys(questions));
-
-
-
-
-// Create click event for answer
-// function answerQuestion (){
-
-    function questionCorrect() {
-        document.getElementById("answerResponse").innerHTML = "Correct";
-    }
-    function questionWrong() {
-        document.getElementById("answerResponse1").innerHTML = "Wrong";
-    }
+var body = document.body;
+var h1El = document.createElement("h1");
+var infoEl = document.createElement("div");
+var questionEl = document.createElement("div");
+// Create ordered list element
+var listEl = document.createElement("ol");
+// Create ordered list items
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+var li0 = document.createElement("ul")
 
 
-
-//question #1 object...    questions[0].question
-var questions= 
-[
-{
-question:  "what is...?",
-answer: "the answer is...",
-button1: "the answer is...",
-button2: "button 2",
-button3: "button 3",
-button4: "button 4",
-},
-{
-  question:  "what is...?",
-  answer: "the answer is...",
-  button1: "the answer is...",
-  button2: "button 2",
-  button3: "button 3",
-  button4: "button 4",
-  }
+// Add text for list items
+li0.textContent = questions[0].question;
+li1.textContent = questions[0].button1;
+li2.textContent = questions[0].button2;
+li3.textContent = questions[0].button3;
+li4.textContent = questions[0].button4;
 
 
+body.appendChild(h1El)
+body.appendChild(questionEl);
+questionEl.appendChild(listEl);
+// Append ordered list 
+questionEl.appendChild(listEl);
+// Append list items to ordered list element 
+listEl.appendChild(li0);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
 
-]
-questions[0].question
-
-
-
-
-
-//    document.getElementById("button1").addEventListener("click", function (){
-//         alert("did this work?")
-//         });
-
-// document.getElementById("button2").addEventListener("click", function (){
-//     alert("did this work?")
-//     });
-// document.getElementById("button3").addEventListener("click", function (){
-//         alert("did this work?")
-//         });
-// document.getElementById("button4").addEventListener("click", function (){
-//     alert("did this work?")
-//             });    
-            
-
-
+// Add styling to list element
+// h1El.setAttribute("class", "question-hero-h1");
+listEl.setAttribute("class", "question-hero");
+// Add styling to list items
+li0.setAttribute("class", "question-title");
+li1.setAttribute("class" , "button" );
+li2.setAttribute("class" , "button" );
+li3.setAttribute("class" , "button" );
+li4.setAttribute("class" , "button" );
